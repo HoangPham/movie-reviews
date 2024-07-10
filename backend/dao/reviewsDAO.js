b720bfea15e1951f4f4f9cba0dcffa67c90720ce
khanhat <kha@trophyroom.io>
2024-07-10 13:07:32 +0700

    Refactor: Streamline ReviewsDAO methods and fix ObjectId usage
    - Corrected the use of `new` with `ObjectId` to prevent TypeError in `updateReview`.
    - Simplified database operation methods (`addReview`, `updateReview`, `deleteReview`) for clarity and efficiency.
    - Ensured consistent error handling across methods.

--- backend/dao/reviewsDAO.js
+++ backend/dao/reviewsDAO.js
@@ -52,11 +52,10 @@ export default class ReviewsDAO {
 
     static async deleteReview(reviewId, userId) {
         try {
-            const deleteResponse = await reviews.deleteOne({
-                _id: ObjectId(reviewId),
+            return await reviews.deleteOne({
+                _id: new ObjectId(reviewId),
                 user_id: userId,
             });
-            return deleteResponse;
         }
         catch (e) {
             console.error(`unable to delete review: ${e}`);
